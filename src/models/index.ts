2e3e169a3806cf174bd4464c2e0b624f903c323f
Joe Velez <joshephet@gmail.com>
2025-09-09 12:46:00 -0500

    feat: add Event, Ticket and Purchase models with separate ticket collection

---
+++ src/models/index.ts
@@ -0,0 +1,4 @@
+export { default as User } from "./User.js";
+export { default as Event } from "./Event.js";
+export { default as Ticket } from "./Ticket.js";
+export { default as Purchase } from "./Purchase.js";

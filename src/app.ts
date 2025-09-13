584edc4a4cc0e171794e39eab134dcc26541f12f
Juan Diaz <jdiazmoreno271@gmail.com>
2025-09-13 17:27:08 -0500

    feat: project connection setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API de TicketHub funcionando!');
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import app from './app';
+import connectDB from './config/database';
 
-dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+};
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Prueba Taller de NodeJS");
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+startServer();
